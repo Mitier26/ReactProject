ab5a12b6de76e7db852e5af7a1fa1509b7dfe480
Molkang <mitier26@gmail.com>
2024-03-23 10:28:41 +0900

    브랜드 페이지

--- brandpage/src/App.jsx
+++ brandpage/src/App.jsx
@@ -1,7 +1,14 @@
 import './App.css';
+import Hero from './components/Hero';
+import Navigation from './components/Navigation';
 
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div>
+            <Navigation />
+            <Hero />
+        </div>
+    );
 }
 
 export default App;
