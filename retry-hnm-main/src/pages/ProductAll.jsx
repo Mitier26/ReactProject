05d36a0841f5a4e62180fa3f6fe4e873058070a6
Molkang <mitier26@gmail.com>
2024-03-31 00:24:48 +0900

    () 빼기

--- retry-hnm-main/src/pages/ProductAll.jsx
+++ retry-hnm-main/src/pages/ProductAll.jsx
@@ -1,36 +1,40 @@
-import React, {useEffect, useState} from "react";
-import ProductCard from "../components/ProductCard";
+import React, { useEffect, useState } from 'react';
+import ProductCard from '../components/ProductCard';
 // import Header from "../components/Header";
 // import Footer from "../components/Footer";
-import { Container, Row, Col } from "react-bootstrap";
-import { useSearchParams } from "react-router-dom";
+import { Container, Row, Col } from 'react-bootstrap';
+import { useSearchParams } from 'react-router-dom';
 
-const ProjectAll = ()=>{
-    
-    const [productList, setProductList]=useState([]);
+const ProjectAll = () => {
+    const [productList, setProductList] = useState([]);
     const [query, setQuery] = useSearchParams();
 
-    const getProducts = async ()=>{
-        let searchQuery = query.get("q") || "";
-        let url = (` https://my-json-server.typicode.com/leesanghun0/retry-hnm/products?q=${searchQuery}`);
+    const getProducts = async () => {
+        let searchQuery = query.get('q') || '';
+        let url = ` https://my-json-server.typicode.com/leesanghun0/retry-hnm/products?q=${searchQuery}`;
         let response = await fetch(url);
         let data = await response.json();
         //console.log(data)
-        setProductList(data)
-    }
+        setProductList(data);
+    };
 
-    useEffect(()=>{
+    useEffect(() => {
         getProducts();
-    },[query])
+    }, [query]);
 
-
-    return(<>
-        <Container>
-            <Row>
-                {productList.map((item, key)=>(<Col key={key} lg={3}>{<ProductCard item={item} />}</Col>))}
-            </Row>
-        </Container>
-    </>)
-}
+    return (
+        <>
+            <Container>
+                <Row>
+                    {productList.map((item, key) => (
+                        <Col key={key} lg={3}>
+                            {<ProductCard item={item} />}
+                        </Col>
+                    ))}
+                </Row>
+            </Container>
+        </>
+    );
+};
 
 export default ProjectAll;
