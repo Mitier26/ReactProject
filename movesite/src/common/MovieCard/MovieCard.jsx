ca8698742184b7658df6f65c340dc379c4f39b1d
Molkang <mitier26@gmail.com>
2024-07-27 23:49:52 +0900

    장르 가지고 오기

--- movesite/src/common/MovieCard/MovieCard.jsx
+++ movesite/src/common/MovieCard/MovieCard.jsx
@@ -1,13 +1,30 @@
 import React from 'react'
 import Badge from 'react-bootstrap/Badge';
 import './MovieCard.style.css'
+import { useMovieGenreQuery } from '../../Hooks/useMovieGenre';
 
 const MovieCard = ({movie}) => {
+
+    const {data:genreData} = useMovieGenreQuery();
+
+    console.log(genreData);
+
+    const showGenre = (genreIdList) => {
+        if(!genreData) return [];
+
+        const genreNameList = genreIdList.map((id)=>{
+            const genreObj = genreData.find((genre)=>genre.id === id)
+            return genreObj.name;
+        })
+
+        return genreNameList;
+    }
+
     return (
         <div className='movie-card' style={{backgroundImage:"url(" +`	https://media.themoviedb.org/t/p/w600_and_h900_bestv2${movie.poster_path}` + ")"}}>
             <div className='overlay'>
                 <h1>{movie.title}</h1>
-                {movie.genre_ids.map((id, index)=><Badge bg='danger' key={index}>{id}</Badge>)}
+                {showGenre(movie.genre_ids).map((id, index)=><Badge bg='danger' key={index}>{id}</Badge>)}
                 <div>
                     <div>{movie.vote_average}</div>
                     <div>{movie.popularity}</div>
