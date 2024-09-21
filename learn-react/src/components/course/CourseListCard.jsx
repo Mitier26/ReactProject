fc83ceed1ae90d4ce9d71082bb87fd79c8195012
Molkang <mitier26@gmail.com>
2024-09-21 21:58:11 +0900

    컴포넌트

--- learn-react/src/components/course/CourseListCard.jsx
+++ learn-react/src/components/course/CourseListCard.jsx
@@ -1,16 +1,55 @@
 import { CourseItem } from "./CourseItem";
+import Card from "../Card";
 
-export default function CourseListCard(props) {
+export default function CourseListCard({items}) {
+
+  const [course1, course2, course3] = items;
+
+  const courses = {
+    course1 : {
+      title: '입문자를 위한, HTML&CSS 웹 개발 입문',
+      description: '웹 개발에 필요한 기본 지식을 배웁니다.',
+      thumbnail: '/img/htmlcss.png',
+    },
+    course2 : {
+      title: '입문자를 위한, ES6+ 최신 자바스크립트 입문',
+      description: '쉽고! 알찬! 내용을 준비했습니다.',
+      thumbnail: '/img/js.png',
+    },
+    course3 : {
+      title: '포트폴리오 사이트 만들고 배포까지!',
+      description: '포트폴리오 사이트를 만들고 배포해 보세요.',
+      thumbnail: '/img/portfolio.png',
+    }
+  }
+
   return (
-      <div className="card">
+    <>
+      <Card title={'강의목록'}>
+        <div className="courses">
+          <CourseItem {...course1}/>
+          <CourseItem {...course2}/>
+          <CourseItem {...course3}/>
+        </div>
+      </Card>
+      {/* <div className="card">
           <div className="card__header">강의목록</div>
           <div className="card__body">
             <div className="courses">
-            <CourseItem/>
-            <CourseItem/>
-            <CourseItem/>
+              <CourseItem {...course1}/>
+              <CourseItem {...course2}/>
+              <CourseItem {...course3}/>
+            {Object.values(courses).map((course, index) => (
+              <CourseItem 
+                key={index}
+                title={course.title}
+                description={course.description}
+                thumbnail={course.thumbnail}
+              />
+            ))}
             </div>
           </div>
-      </div>
+      </div> */}
+      </>
   );
 }
