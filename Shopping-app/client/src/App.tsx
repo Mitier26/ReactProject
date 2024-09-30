4285f3d9a08a0af1a75f710e9cb062172885f0e8
Molkang <mitier26@gmail.com>
2024-09-30 17:53:15 +0900

    state 확인

--- Shopping-app/client/src/App.tsx
+++ Shopping-app/client/src/App.tsx
@@ -1,8 +1,37 @@
 import './App.css';
+import { useState } from 'react';
+
+const Child = ({ state, action }: any) => {
+  return (
+    <button type="button" onClick={action}>
+      {state}
+    </button>
+  );
+};
+
+const Child2 = ({ state }: any) => {
+  return <button>{state}</button>;
+};
 
 function App() {
+  const [number, setNumber] = useState(0);
+  const [number1, setNumber1] = useState(0);
+  const [number2, setNumber2] = useState(0);
+
+  const increase = () => setNumber(number + 1);
+
+  const handleChange = () => {
+    setNumber1((prev) => prev + 2);
+    setNumber2((prev) => prev + 3);
+  };
+
   return (
-    <h1>쇼필몰 앱 만들어보기</h1>
+    <>
+      <Child state={number} action={increase} />
+      <Child2 state={number1} />
+      <Child2 state={number2} />
+      <button onClick={handleChange}>Change</button>
+    </>
   );
 }
 
